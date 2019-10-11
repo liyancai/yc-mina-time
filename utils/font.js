9b00e0095e73998e14178486d48114dc2de84f31
liyancai <liyancai1986@163.com>
2019-10-11 15:14:48 +0800

    @

--- utils/font.js
+++ utils/font.js
@@ -3,18 +3,17 @@
  */
 let loadFontFace = () => {
 
-  let filePath = wx.env.USER_DATA_PATH + '/YournameD7HomeHalf.ttf'
   // 液晶字体
-  wx.downloadFile({
-    // fileID: 'cloud://release-zm8us.7265-release-zm8us/YournameD7HomeHalf.ttf', // 云存储文件 ID
-    url: 'https://7265-release-zm8us-1259190818.tcb.qcloud.la/YournameD7HomeHalf.ttf?sign=d2f42e24e64cbbe9c0c9eda5c8852fec&t=1570713860',
-    filePath: filePath,
+  wx.cloud.downloadFile({
+    fileID: 'cloud://release-zm8us.7265-release-zm8us/YournameD7HomeHalf.ttf', // 云存储文件 ID
+    // url: 'https://7265-release-zm8us-1259190818.tcb.qcloud.la/YournameD7HomeHalf.ttf?sign=d2f42e24e64cbbe9c0c9eda5c8852fec&t=1570713860',
+    // filePath: filePath,
     success: res => {
-      console.debug(res)
+      console.info(res)
       wx.loadFontFace({
         family: 'YournameD7HomeHalf', //font-family使用的名字
-        // source: 'url("' + res.tempFilePath + '")', //字体资源的地址
-        source: 'url("' + filePath + '")', //字体资源的地址
+        source: 'url("' + res.tempFilePath + '")', //字体资源的地址
+        // source: 'url("' + res.filePath + '")', //字体资源的地址
         success: console.log
       })
     },
